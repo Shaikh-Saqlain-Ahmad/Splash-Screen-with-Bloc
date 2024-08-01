af7d223349d8a05b0538b18b31dd26f5151ab4e4
Saqlain Ahmad <saqlainahmed292@gmail.com>
2024-08-01 16:25:46 +0500

    login screen code refactored

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:splash_screen_with_bloc/model/user/user_model.dart';
+import 'package:splash_screen_with_bloc/view/login/widget/widgets.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,7 +11,9 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   UserModel userModel = UserModel();
-
+  final emailFocusNode = FocusNode();
+  final passwordFocusNode = FocusNode();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +21,23 @@ class _LoginScreenState extends State<LoginScreen> {
         title: const Text("Login Screen"),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Login screen after splash screen"),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            EmailInputWidget(emailFocusNode: emailFocusNode),
+            const SizedBox(
+              height: 20,
+            ),
+            PasswordInputWidget(passwordFocusNode: passwordFocusNode),
+            const SizedBox(
+              height: 20,
+            ),
+            LoginButton(formKey: _formKey)
+          ],
+        ),
       ),
     );
   }
